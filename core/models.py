824aac5928311918063b7b7e4e5ebcc76f832f4c
Euclides <euclideskinto@gmail.com>
2022-01-06 20:20:30 -0300

    add django-localflavor

--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
+from __future__ import unicode_literals
 from django.db import models
-
-# Create your models here.
+from django.contrib import admin
+from django.utils.html import format_html
+from django.db.models import Sum,Avg
+from localflavor.br.br_states import STATE_CHOICES
+from 
