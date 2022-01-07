04f9ddbe9f9b100340db569b77545f9f8fe706bb
Euclides <euclideskinto@gmail.com>
2022-01-06 21:20:18 -0300

    add admin Customer e ProposalAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
+from django import forms
 from django.contrib import admin
+from .models import Customer, Proposal
+from django.utils.html import format_html
+from django.contrib.admin.views.main import ChangeList
+from django.db.models import Sum, Avg
 
-# Register your models here.
+
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = ["customer", "date", "paid", "value"]
+    search_fields = ["customer__name"]
+    list_per_page = 30
+    save_on_top = True
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Proposal, ProposalAdmin)
+admin.site.register(Customer, CustomerAdmin)
